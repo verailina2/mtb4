9b2e3508d1e51e4395c6f086da255a2f88ce73da
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-05-10 14:30:28 +0300

    Выполнено задание с функцияй

--- Homework.py
+++ Homework.py
@@ -10,3 +10,5 @@ print(get_divisible_numbers([1, 2, 3, 4, 5, 6], 2))  # [2, 4, 6]
 print(get_divisible_numbers([0, 1, 2, 3, 4, 5, 6], 4))  # [0, 4]
 print(get_divisible_numbers([0], 4))  # [0]
 print(get_divisible_numbers([1, 3, 5], 2)) 
+
+# Выполнено
