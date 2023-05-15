b01be1f14e73bd8a649541f9b4ca5af6234568f3
lizobakted <lizobakt.ed@yandex.ru>
2023-05-15 10:45:46 +0300

    Переписала функцию find_digit_in_number

--- function_check_digit.py
+++ function_check_digit.py
@@ -1,18 +1,19 @@
-def find_digit_in_number():
-    number = input('Введите число:')
-    index1 = int(input('Введите первый индекс:'))
-    index2 = int(input('Введите второй индекс:'))
-    digit = input('Введите цифру, которую нужно искать в заданном числе:')
+def find_digit_in_number(number, index1, index2, digit):
     min_index, max_index = sorted([index1, index2])
+
     if digit in number[min_index:max_index + 1]:
-        print(f"Цифра {digit} есть в числе {number} между индексами {min_index} и {max_index}")
+        return True
     else:
-        print(f"Цифры {digit} нет в числе {number} между индексами {min_index} и {max_index}")
+        return False
 
 
-print(find_digit_in_number())
+number = input('Введите число:')
+index1 = int(input('Введите первый индекс:'))
+index2 = int(input('Введите второй индекс:'))
+digit = input('Введите цифру, которую нужно искать в заданном числе:')
 
-# Да, действительно следующее задание – представить решение задачи в виде функции.
-# Но конечно мы бы хотели убрать конструкции с input из функции (либо вынести их, либо удалить)
-# и зафиксировать возвратное значение из фукцнии через return как True или False
-# Также в функцию надо передать параметры)
+result = find_digit_in_number(number, index1, index2, digit)
+if result:
+    print(f"Цифра {digit} есть в числе {number} между индексами {min_index} и {max_index}")
+else:
+    print(f"Цифры {digit} нет в числе {number} между индексами {min_index} и {max_index}")
